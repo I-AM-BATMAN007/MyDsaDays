27f86e723877f176f063f587d8083ea47347dfe4
I-AM-BATMAN007 <choudharyrajpreet@gmail.com>
2024-07-10 22:06:30 +0530

    new folder

---
+++ Searching.cpp/IterativeBinarySearch.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int key;
+    cin>>key;
+    int low=0;
+    int high=n-1;
+
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]==key){
+            cout<<mid;
+            break;
+        }
+        else if(arr[mid]<key){
+            high=mid-1;
+        }
+        else if(arr[mid]>key){
+            low=mid+1;
+        }
+    }
